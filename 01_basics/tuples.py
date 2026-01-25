d251989daa2349e75c7bc657c5af60fab0bb62cf
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-25 10:38:29 +0530

    in python tuples are immutable once created they are not mutable and they are having only 2 fucntions count and index and tuples are faster than the list

---
+++ 01_basics/tuples.py
@@ -0,0 +1,22 @@
+## tuples are faster compared to lists
+## tuples are immutable (cannot be changed after creation)
+# Creating a tuple
+
+tup = (1,2,3,4,500,6,7,8,9,10)
+print(tup)
+print(type(tup))
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+print(tup[2:8])
+print(tup[::-1])
+print(tup[:])
+print(tup[1:10:2])
+# tup[0] = 99 # This will raise an error because tuples are immutable
+print(tup)
+
+#tuple object does not support item assignment
+# tup[0]=  99
+print(tup)
+print(tup.index(500))
+print(tup.count(9))
