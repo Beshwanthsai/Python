3b9bc2a392ea71fc0caad76971ada6cc287793ea
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-25 10:16:12 +0530

    list file modified with some basics in list in python

---
+++ 01_basics/list.py
@@ -0,0 +1,24 @@
+nums = [12,25,37,49,58,69,72,84,1,100]
+print(nums)
+
+print(type(nums))
+
+print(len(nums))
+
+print(nums[0])
+
+print(nums[-1])
+
+print(nums[-2])
+
+print(nums[-5]) #print from the end
+
+print(nums[2:8]) #print from index 2 to index 7
+print(nums[5:]) #prints from index 5 to end
+print(nums[:4]) #prints from start to index 3
+
+print(nums[::3]) #print every third element
+print(nums[::-1]) #prints the list in reverse order
+
+print(nums[:]) #print the whole list
+print(nums[1:10:2]) #print from index 1 to index 8 with a step of 2
