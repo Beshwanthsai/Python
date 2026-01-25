d74a6d9bd10c9a823b9877a36290c92762bb865a
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-01-25 10:47:27 +0530

    set functions

--- 01_basics/set.py
+++ 01_basics/set.py
@@ -29,3 +29,29 @@ s.clear()
 print(s)
 print(len(s))
 
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+print(s1|s2)
+print(s1.union(s2))
+print(s1&s2)
+print(s1.intersection(s2))
+
+print(s1 - s2)
+print(s1.difference(s2))
+
+print(s2 - s1)
+print(s2.difference(s1))
+print(s1 ^ s2)
+print(s1.symmetric_difference(s2))
+print(s1.isdisjoint(s2))
+print(s1.issubset(s2))
+print(s1.issuperset(s2))
+s3 = {1,2}
+print(s3.issubset(s1))
+print(s1.issuperset(s3))
+s4 = s1.copy()
+print(s4)
+s1.clear()
+print(s1)
+print(s4)
+
