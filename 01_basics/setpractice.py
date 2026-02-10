6bafc6d68df7a195296f674277270f6c03906835
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-10 18:19:17 +0530

    checking if the set contains any duplicates or not

---
+++ 01_basics/setpractice.py
@@ -0,0 +1,6 @@
+my_set = {1,2,3,3,3,4,5,6,7,8,9}
+print(my_set)
+#thia saya thst the set will only contains unique eklements and will not contain duplicates. So the output will be {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+for x in range(my_set):
+    print(x)
