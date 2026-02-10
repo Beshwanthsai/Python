f5eec2838c872dd65cfac78390cf9f2c516c0d20
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-10 18:24:32 +0530

    set functions

--- 01_basics/setpractice.py
+++ 01_basics/setpractice.py
@@ -2,5 +2,21 @@ my_set = {1,2,3,3,3,4,5,6,7,8,9}
 print(my_set)
 #thia saya thst the set will only contains unique eklements and will not contain duplicates. So the output will be {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-for x in range(my_set):
+for x in my_set:
     print(x)
+
+my_set.add(2323)
+print(my_set)
+my_set.add(2323)
+print(my_set)
+my_set.remove(3)
+print(my_set)
+my_set.discard(4)
+print(my_set)
+my_set.update({232,234,235})
+print(my_set)
+my_set.update([100,200,300])
+print(my_set)
+my_set.update((400,500,600))
+print(my_set)
+print(len(my_set))
