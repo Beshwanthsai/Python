389330cfaaecd296d5256ec12165f5383554e309
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2026-02-10 15:12:05 +0530

    formatting in python

--- 01_basics/format.py
+++ 01_basics/format.py
@@ -2,3 +2,13 @@ firstname = "Beshwanth Sai"
 lastname = "Katari"
 sentence = "My name is {} {}"
 print(sentence.format(firstname,lastname))
+
+name  = "Beshwanth Sai Katari"
+age = 21
+School = "Sri Srinivasa High School"
+college = "Sri Srinivasa Gravitty College"
+
+sentence = (f"MY name is {name} and i am {age} years old. I studied in {School} and currently i am st"
+            f"udying in {college}")
+
+print(sentence)
